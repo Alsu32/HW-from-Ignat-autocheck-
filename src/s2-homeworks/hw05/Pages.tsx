fddeb871f46e36f18dc8ea2c49ab6e37e2f34bd4
Alsu Kirillova <sai-alsu@mail.ru>
2022-11-15 16:57:09 +0300

    HW5(3)

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import {Routes, Route, Navigate} from 'react-router-dom'
+import {Routes, Route, Navigate, Outlet} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
 
 
-
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+    JUNIOR_PLUS: '/-junior-plus',
 }
 
 function Pages() {
@@ -22,23 +21,30 @@ function Pages() {
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 <Route
                     path='/'
-                    element={<PreJunior/>}
-                />
+                    element={(
+                        <>
+                            <PreJunior/>
+                            <Outlet/>
+                        </>)}>
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route
-                    path={PATH.PRE_JUNIOR}
-                    element={<PreJunior/>}
-                />
-                <Route
-                    path={PATH.JUNIOR}
-                    element={<Junior/>}
+                    {/*роуты для /pre-junior, /junior, /junior-plus*/}
+                        <Route
+                            path={PATH.PRE_JUNIOR}
+                            element={<PreJunior/>}
+                        />
+                        <Route
+                            path={PATH.JUNIOR}
+                            element={<Junior/>}
 
-                />
-                <Route
-                    path={PATH.JUNIOR_PLUS}
-                    element={<JuniorPlus/>}
-                />
+                        />
+                        <Route
+                            path={PATH.JUNIOR_PLUS}
+                            element={<JuniorPlus/>}
+                        />
+
+
+                </Route>
+
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
                 <Route
